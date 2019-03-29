7789708f63f1614b94de91bb0386566ce2731bfc
DaytonS3 <Daytonrsteinmeyer@gmail.com>
2019-03-29 11:48:58 -0400

    Cards render to screen

--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -1,15 +1,16 @@
 import React from 'react';
+import propTypes from 'prop-types';
 
 const Tab = props => {
   /* Using your props, determine if the `tab` prop matches the `selectedTab` prop, 
       if they match, the className should be: 'tab active-tab', 
       if it is not it should just be 'tab'*/
+      //console.log(props)
   return (
     <div
-      className={''}
+      className={props.tab === props.selectedTab ? 'tab active-tab' : 'Tab' }
       onClick={() => {
-        /* Replace this dummy click handler function with your selectTabHandler function from props 
-         you'll need to pass the `tab` in as an argument to this handler. */
+        props.selectHandler(props.tab)
       }}
     >
       {props.tab.toUpperCase()}
@@ -18,5 +19,8 @@ const Tab = props => {
 };
 
 // Make sure you include PropTypes on your props.
+Tab.propTypes = {
+  tab: propTypes.string
+}
 
 export default Tab;
